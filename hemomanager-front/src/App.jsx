504861ab9cb9d90bfdf9ad2c463c0fce7d50e995
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-03 11:20:17 -0300

    Rendering quiz component to test

--- hemomanager-front/src/App.jsx
+++ hemomanager-front/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { PerfilUsuario } from "./components/PerfilUsuario";
+import { Quiz } from "./components/Quiz";
 import { GlobalStyled } from "./styles/global";
 
 export function App() {
   return (
     <>
-      <PerfilUsuario />
-      <GlobalStyled />
+      <Quiz />
     </>
   );
 }
