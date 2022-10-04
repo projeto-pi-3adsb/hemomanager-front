cce7fa59e8c941535553f06c531ccb7ce5d9222b
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-04 14:35:51 -0300

    created quiz page

--- hemomanager-front/src/pages/QuizPage/styles.js
+++ hemomanager-front/src/pages/QuizPage/styles.js
@@ -1,8 +1,47 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: red;
-  height: 90vh;
-  width: 90vw;
+  padding: 1rem;
+  margin: auto;
+  margin-top: 1rem;
+  height: 85vh;
+  width: 80vw;
   display: block;
 `;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: start;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    align-items: center;
+
+    span {
+      font-weight: 700;
+      color: var(--red-blood);
+      font-size: 1.2rem;
+    }
+  }
+
+  img {
+    align-self: flex-start;
+    height: 4rem;
+  }
+
+  p{
+    margin-top: 1rem;
+    font-size: 1.4rem;
+    font-family: "Roboto", sans-serif;
+  }
+`;
+
+export const Title = styled.strong`
+  font-family: "Francois One", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  color: var(--black);
+`;
+
+
