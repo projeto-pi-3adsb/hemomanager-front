f1d08757e173fcb39d31de92ccc06fceaa9b77af
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-06 13:17:34 -0300

    Other changes

--- hemomanager-front/src/pages/QuizPage/index.jsx
+++ hemomanager-front/src/pages/QuizPage/index.jsx
@@ -1,55 +1,51 @@
-import React, { useContext } from "react";
-import { ButtonComeback } from "../../components/ButtonComeback";
+import React, { useState } from "react";
 import { HeaderComponent } from "../../components/Header";
 import { Container, Header, Title } from "./styles";
 import bloodDrop from "../../assets/blood-drop.svg";
-import { Question, QuestionContext } from "../../components/Question";
+import { Question } from "../../components/Question";
 
 const questions = [
   {
+    id: 1,
     ask: "Você tem entre 16 e 69 anos?",
     answer: false,
   },
   {
+    id: 2,
     ask: "Passou por quadros de gripe, resfriado ou febre nos últimos 7 dias?",
     answer: false,
   },
   {
+    id: 3,
     ask: "Foi diagnosticado(a) com Covid-19 nos últimos 30 dias?",
     answer: false,
   },
   {
+    id: 4,
     ask: "Fez tatuagem nos últimos 12 meses?",
     answer: false,
   },
   {
+    id: 5,
     ask: "Pesa no mínimo 50kg?",
     answer: false,
   },
   {
+    id: 6,
     ask: "Dormiu, pelo menos, 6 horas nas últimas 24 horas?",
     answer: false,
   },
 ];
 
-const asks = []
-const answers = []
 
-questions.map((question)=>{
-  asks.push(question.ask)
-  answers.push(question.answer)
-})
 
 export function QuizPage() {
-  const questionContext = useContext(QuestionContext);
-
-  const { counter } = questionContext;
-
-  console.log("Counter: " + counter);
+  const [counter, setCounter] = useState(0);
+  const [answer, setAnswer] = useState(false);
 
   return (
     <>
-      <HeaderComponent button={<ButtonComeback />} />
+      <HeaderComponent button={true} />
       <Container>
         <Header>
           <div>
@@ -59,8 +55,20 @@ export function QuizPage() {
           <Title>VOCÊ ESTÁ APTO A DOAR SANGUE?</Title>
           <p>Faça o teste e descubra!</p>
         </Header>
-        <Question question={asks[counter]} />
-        {console.log("Ansower: " + asks[counter])}
+
+        <Question
+          setCounter={setCounter}
+          setAnswer={setAnswer}
+          counter={counter}
+          indexQuestion={questions[counter].ask}
+          answer={answer}
+        />
+
+
+        {console.log("Asks: " + questions[counter].ask)}
+        {console.log(
+          "-------------------------------------"
+        )}    
       </Container>
     </>
   );
