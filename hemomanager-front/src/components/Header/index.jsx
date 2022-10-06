1232d1030f8db227901af319b6fc2702ff4db1aa
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-06 13:12:37 -0300

    was altered rederizing component Button Comeback

--- hemomanager-front/src/components/Header/index.jsx
+++ hemomanager-front/src/components/Header/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { BorderlessButton } from "../BorderlessButton";
 import { ButtonGroup, Header } from "./styles";
 import logo from "../../assets/logotypes/logo-2.png";
+import { ButtonComeback } from "../ButtonComeback";
 
 export function HeaderComponent(props) {
   return (
     <Header>
-      {props.button}
+      {props.button ? <ButtonComeback /> : false}
       <ul>
         <img src={logo} alt="" />
         <li>Home</li>
