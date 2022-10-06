2bea5586aee0af3931229833b108121c26c93522
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-06 13:15:36 -0300

    Properties altered to get data Quiz Page component

--- hemomanager-front/src/components/Question/index.jsx
+++ hemomanager-front/src/components/Question/index.jsx
@@ -1,37 +1,31 @@
-import React, { useState, createContext } from "react";
+import React from "react";
 import { BorderlessButton } from "../BorderlessButton";
 import { Container } from "./styles";
 
-export const QuestionContext = createContext({});
-
 export function Question(props) {
-  
-  const [counter, setCounter] = useState(0);
-  const [answer, setAnswer] = useState(false);
-  
+
+
   function doAnswerTrue() {
-    setAnswer(true);
-    setCounter(counter + 1);
+
+    props.setAnswer(true)
+    props.setCounter(props.counter + 1);
   }
-  
+
   function doAnswerFalse() {
-    setAnswer(false);
-    setCounter(counter + 1);
+
+    props.setAnswer(false);
+    props.setCounter(props.counter + 1);
+
   }
 
   return (
-    <QuestionContext.Provider value={counter}>
-      <Container>
-        <h4>{props.question}</h4>
-        <div>
-          <BorderlessButton text="Sim" method={doAnswerTrue} />
+    <Container>
+      <h4>{props.indexQuestion}</h4>
+      <div>
+        <BorderlessButton text="Sim" method={doAnswerTrue} />
 
-          <BorderlessButton text="Não" method={doAnswerFalse} />
-
-          {console.log(counter)}
-          {console.log(answer)}
-        </div>
-      </Container>
-    </QuestionContext.Provider>
+        <BorderlessButton text="Não" method={doAnswerFalse} />
+      </div>
+    </Container>
   );
 }
