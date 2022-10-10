ec75e51da723721a7ad3beb2ec935256f7e91f79
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-10-09 22:45:31 -0300

    Indentation

--- hemomanager-front/src/components/shared/Cards/CardMeusAgend.jsx
+++ hemomanager-front/src/components/shared/Cards/CardMeusAgend.jsx
@@ -1,90 +1,74 @@
 import React from "react";
 import {
-    BoxCard,
-    BoxImg,
-    BoxText,
-    Title,
-    SubTitle,
-    Description,
-    BoxButton,
-    Button,
-    ListCard
-} from "./styles"
+  BoxCard,
+  BoxImg,
+  BoxText,
+  Title,
+  SubTitle,
+  Description,
+  BoxButton,
+  Button,
+  ListCard,
+} from "./styles";
 
-import Maps from "../../../assets/maps.png"
+import Maps from "../../../assets/maps.png";
 
 export function CardMeusAgend(props) {
-    return (<>
-        <ListCard>
+  return (
+    <>
+      <ListCard>
         <BoxCard>
-        <BoxImg>
+          <BoxImg>
             <img src={Maps} id="mapa" />
-        </BoxImg>
-        <BoxText>
-            <Title>
-                HEMOCENTRO FLOR DO CAMPO
-            </Title>
+          </BoxImg>
+          <BoxText>
+            <Title>HEMOCENTRO FLOR DO CAMPO</Title>
             <SubTitle>
-                SÃO PAULO - SP,  Rua Haddock Lobo 565, Vila Brasilândia
+              SÃO PAULO - SP, Rua Haddock Lobo 565, Vila Brasilândia
             </SubTitle>
             <Description>
-                asdfghhgfdsadj
-                asdfghgfdsaasd
-                asdfggbfvdcsxs
+              asdfghhgfdsadj asdfghgfdsaasd asdfggbfvdcsxs
             </Description>
-        </BoxText>
-        <BoxButton>
-            <Button>
-                CANCELAR AGENDAMENTO
-            </Button>
-        </BoxButton>
+          </BoxText>
+          <BoxButton>
+            <Button>CANCELAR AGENDAMENTO</Button>
+          </BoxButton>
         </BoxCard>
         <BoxCard>
-        <BoxImg>
+          <BoxImg>
             <img src={Maps} id="mapa" />
-        </BoxImg>
-        <BoxText>
-            <Title>
-                HEMOCENTRO FLOR DO CAMPO
-            </Title>
+          </BoxImg>
+          <BoxText>
+            <Title>HEMOCENTRO FLOR DO CAMPO</Title>
             <SubTitle>
-                SÃO PAULO - SP,  Rua Haddock Lobo 565, Vila Brasilândia
+              SÃO PAULO - SP, Rua Haddock Lobo 565, Vila Brasilândia
             </SubTitle>
             <Description>
-                asdfghhgfdsadj
-                asdfghgfdsaasd
-                asdfggbfvdcsxs
+              asdfghhgfdsadj asdfghgfdsaasd asdfggbfvdcsxs
             </Description>
-        </BoxText>
-        <BoxButton>
-            <Button>
-                CANCELAR AGENDAMENTO
-            </Button>
-        </BoxButton>
+          </BoxText>
+          <BoxButton>
+            <Button>CANCELAR AGENDAMENTO</Button>
+          </BoxButton>
         </BoxCard>
         <BoxCard>
-        <BoxImg>
+          <BoxImg>
             <img src={Maps} id="mapa" />
-        </BoxImg>
-        <BoxText>
-            <Title>
-                HEMOCENTRO FLOR DO CAMPO
-            </Title>
+          </BoxImg>
+          <BoxText>
+            <Title>HEMOCENTRO FLOR DO CAMPO</Title>
             <SubTitle>
-                SÃO PAULO - SP,  Rua Haddock Lobo 565, Vila Brasilândia
+              SÃO PAULO - SP, Rua Haddock Lobo 565, Vila Brasilândia
             </SubTitle>
             <Description>
-                asdfghhgfdsadj
-                asdfghgfdsaasd
-                asdfggbfvdcsxs
+              asdfghhgfdsadj asdfghgfdsaasd asdfggbfvdcsxs
             </Description>
-        </BoxText>
-        <BoxButton>
-            <Button>
-                CANCELAR AGENDAMENTO
-            </Button>
-        </BoxButton>
+          </BoxText>
+          <BoxButton>
+            <Button>CANCELAR AGENDAMENTO</Button>
+          </BoxButton>
         </BoxCard>
-        </ListCard>   
-     </>)
+      </ListCard>
+    </>
+  );
 }
--- hemomanager-front/src/components/shared/Cards/styles.js
+++ hemomanager-front/src/components/shared/Cards/styles.js
@@ -1,94 +1,79 @@
 import styled from "styled-components";
 
- export const BoxCard = styled.div`
-    height : 80vh;
-    width: 25vw;
-    display: flex;
-    flex-direction:column;
+export const BoxCard = styled.div`
+  height: 80vh;
+  width: 25vw;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #c5c5c6;
+`;
+
+export const BoxImg = styled.div`
+  #mapa {
+    margin-top: 3px;
+    margin-bottom: 10px;
+    width: 100%;
     border-radius: 10px;
-    border-width: 1px;
-    border-style: solid;
-    border-color: #C5C5C6;
-
- ` 
-
- export const BoxImg = styled.div`
-
- #mapa {
-   margin-top:3px;
-   margin-bottom:10px;
-   width:100%;
-   border-radius: 10px;
- }
-
-` 
+  }
+`;
 
 export const BoxText = styled.div`
-height : 30%;
-width: 100%;
-margin-left:10px;
-
-
-` 
+  height: 30%;
+  width: 100%;
+  margin-left: 10px;
+`;
 
 export const Title = styled.h1`
-border-style: solid;
-border-bottom-width: 1px;
-border-top-width: 0;
-border-right-width: 0;
-border-left-width: 0;
-width:80%;
-margin-bottom:10px;
-
-
-
-` 
+  border-style: solid;
+  border-bottom-width: 1px;
+  border-top-width: 0;
+  border-right-width: 0;
+  border-left-width: 0;
+  width: 80%;
+  margin-bottom: 10px;
+`;
 
 export const SubTitle = styled.h2`
-font-weight: 300;
-margin-bottom:10px;
-color: #00000;
-font-size: 0.8em;
-
-` 
+  font-weight: 300;
+  margin-bottom: 10px;
+  color: #0000;
+  font-size: 0.8em;
+`;
 export const Description = styled.h3`
-color: #666666;
-font-size: 0.8em;
-font-weight: 300;
-
-` 
+  color: #666666;
+  font-size: 0.8em;
+  font-weight: 300;
+`;
 
 export const BoxButton = styled.div`
-height : 20%;
-width: 100%;
-display: flex;
-justify-content: end;
-
-
-`
+  height: 20%;
+  width: 100%;
+  display: flex;
+  justify-content: end;
+`;
 
 export const Button = styled.button`
-height : 40%;
-width: 70%;
-align-self: end;
-margin-right:10px;
-margin-bottom:10px;
-border-radius: 5px;
-border-width: 1px;
-border-style: solid;
-border-color: #C5C5C6;
-
-
-`
+  height: 40%;
+  width: 70%;
+  align-self: end;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #c5c5c6;
+`;
 
 export const ListCard = styled.div`
-height : 70vh;
-width: 100%;
-display: flex;
-justify-content:space-evenly;
-margin-bottom: 150px;
-
-`
+  height: 70vh;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 150px;
+`;
 
 export const BoxTitulo = styled.div`
   align-items: center;
@@ -96,48 +81,41 @@ export const BoxTitulo = styled.div`
   justify-content: space-between;
   height: 80px;
   width: 100%;
-  margin-bottom:20px;
+  margin-bottom: 20px;
 `;
 
-export const MeusAgend = styled.h1 `
-margin-left: 80px;
-font-size: 20px;
-`
+export const MeusAgend = styled.h1`
+  margin-left: 80px;
+  font-size: 20px;
+`;
 
-export const BoxButtonAgend = styled.div `
-height : 60%;
-width: 10%;
-display: flex;
-margin-right:80px;
-`
-export const ButtonAgendEsquerda = styled.button `
-height : 100%;
-width: 50%;
-display: flex;
-background-color: #B83B25;
-margin-right: 10px;
-border-bottom-width: 0;
-border-top-width: 0;
-border-right-width: 0;
-border-left-width: 0;
-border-radius: 5px;
+export const BoxButtonAgend = styled.div`
+  height: 60%;
+  width: 10%;
+  display: flex;
+  margin-right: 80px;
+`;
+export const ButtonAgendEsquerda = styled.button`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  background-color: #b83b25;
+  margin-right: 10px;
+  border-bottom-width: 0;
+  border-top-width: 0;
+  border-right-width: 0;
+  border-left-width: 0;
+  border-radius: 5px;
+`;
 
-`
-
-export const ButtonAgendDireita = styled.button `
-height : 100%;
-width: 50%;
-display: flex;
-background-color: #000000;
-border-bottom-width: 0;
-border-top-width: 0;
-border-right-width: 0;
-border-left-width: 0;
-border-radius: 5px;
-
-
-`
-
-
-
-
+export const ButtonAgendDireita = styled.button`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  background-color: #000000;
+  border-bottom-width: 0;
+  border-top-width: 0;
+  border-right-width: 0;
+  border-left-width: 0;
+  border-radius: 5px;
+`;
