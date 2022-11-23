ed920988a00caeb1d95e9a1847f343250a48d2a5
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-11-23 00:09:02 -0300

    Component to register a new Hemocenter

---
+++ hemomanager-front/src/components/RegisterHemocenter/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 80%;
+  height: 80%;
+  padding: 2rem;
+  border: 1px solid var(--background-border-bar);
+  border-radius: 8px;
+  margin-bottom: 1rem;
+  padding-top: 3rem;
+  overflow-x: hidden;
+
+  select {
+    inline-size: 100%;
+    block-size: 3rem;
+    border: none;
+    border-bottom: 2px solid var(--background-border-bar);
+    background: transparent;
+    font-weight: 400;
+    font-size: 1rem;
+
+    color: var(--gray);
+
+    & + input {
+      margin-block-start: 1rem;
+    }
+
+    &:focus-within {
+      border: none;
+    }
+  }
+
+  input {
+    min-width: 100%;
+    height: 1rem;
+    padding-left: 0;
+    color: var(--black);
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: column;
+    height: 4rem;
+    margin-top: 0.8rem;
+
+    small {
+      background: red;
+    }
+  }
+`;
