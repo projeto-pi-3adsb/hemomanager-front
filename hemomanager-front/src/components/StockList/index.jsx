3d38aa7090188871fa147afa11db25aaaa35815d
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-11-27 05:14:59 -0300

    Getting api datas

--- hemomanager-front/src/components/StockList/index.jsx
+++ hemomanager-front/src/components/StockList/index.jsx
@@ -1,58 +1,35 @@
+import { useEffect, useState } from "react";
+import { api } from "../../api";
 import { BorderlessButton } from "../shared/BorderlessButton";
 import { Container } from "./styles";
 
-const stockList = [
-  {
-    typeBlood: "A+",
-    date: new Date().getDate(),
-    fullHour: {
-      hour: new Date().getHours(),
-      minutos: new Date().getMinutes().toFixed(0),
-    },
-  },
-  {
-    typeBlood: "B+",
-    date: new Date().getDate(),
-    fullHour: {
-      hour: new Date().getHours(),
-      minutos: new Date().getMinutes().toFixed(0),
-    },
-  },
-  {
-    typeBlood: "AB+",
-    date: new Date().getDate(),
-    fullHour: {
-      hour: new Date().getHours(),
-      minutos: new Date().getMinutes().toFixed(0),
-    },
-  },
-];
-
 export function StockList({ isOpen }) {
+  const [bags, setBags] = useState([]);
+
+  useEffect(() => {
+    api
+      .get(`/stock/bloodType/${sessionStorage.id}`)
+      .then((data) => setBags(data.data));
+  }, [setBags]);
+
+
   return (
     <Container>
       <h1>
-        <BorderlessButton
-          doSomething={isOpen}
-          text="INSERIR BOLSA"
-        />
+        <BorderlessButton doSomething={isOpen} text="INSERIR BOLSA" />
       </h1>
       <table>
         <thead>
           <tr>
             <th>Tipo Sanguíneo</th>
             <th>Data da Coleta</th>
-            <th>Hora da Coleta</th>
           </tr>
         </thead>
         <tbody>
-          {stockList.map((bag) => (
+          {bags.map((bag) => (
             <tr>
               <td>{bag.typeBlood}</td>
               <td>{bag.date}</td>
-              <td>
-                {bag.fullHour.hour} : {bag.fullHour.minutos}
-              </td>
             </tr>
           ))}
         </tbody>
