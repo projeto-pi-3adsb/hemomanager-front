f028655963a0e529bb49e6c7a2d17359e7bdd1d5
CindyKanashiro <cindy.goncalves@bandtec.com.br>
2022-11-28 04:32:21 -0300

    formulário do hotsite feito

--- hemomanager-front/src/pages/HotsitePage/HotsitePage.jsx
+++ hemomanager-front/src/pages/HotsitePage/HotsitePage.jsx
@@ -4,8 +4,11 @@ import { Container,  InputGroup ,Header, Title } from "./styles";
 import bloodDrop from "../../assets/blood-drop.svg";
 import { Border } from "../../components/shared/Border/Border";
 import { Input } from "../../components/shared/Input";
+import { BorderlessButton } from "../../components/shared/BorderlessButton";
+import { useNavigate } from "react-router-dom";
 
 export function HotsitePage() {
+    const navigate = useNavigate();
     const [name, setName] = useState(sessionStorage.getItem("user"));
     const [email, setEmail] = useState(sessionStorage.getItem("email"));
     const [phone, setPhone] = useState(sessionStorage.getItem("phone"));
@@ -21,6 +24,12 @@ export function HotsitePage() {
     {
       console.log(user);
     }
+
+    function doGoToRegister() {
+      navigate("/area-usuario");
+      sessionStorage.setItem("page", 1);
+    }
+
   return (
     <>
       <HeaderComponent button={true} />
@@ -39,17 +48,30 @@ export function HotsitePage() {
           <Input valueInput={name} doSomething={setName} />
         </div>
         <div>
-          <span>Email</span>
-          <Input valueInput={email} doSomething={setEmail} />
-        </div>
-        <div>
           <span>Celular</span>
           <Input valueInput={phone} doSomething={setPhone} />
         </div>
         <div>
-          <span>Sexo</span>
+          <span>Idade de 18 a 69 anos</span>
+          <Input valueInput={email} doSomething={setEmail} />
+        </div>
+        <div>
+          <span>Tem mais de 50 quilos?</span>
           <Input valueInput={sex} doSomething={setSex} />
         </div>
+        <div>
+          <span>Fez doação de sangue nos últimos 6 meses?</span>
+          <Input valueInput={sex} doSomething={setSex} />
+        </div>
+        <div>
+          <span>Teve 3 ou mais gestações?</span>
+          <Input valueInput={sex} doSomething={setSex} />
+        </div>
+        <div>
+          <span>Fez o uso de ácido acetilsalicílico ou anti-inflamatórios?</span>
+          <Input valueInput={sex} doSomething={setSex} />
+        </div>
+        <BorderlessButton doSomething={() => doGoToRegister()}text="ENVIAR" />
       </InputGroup>
       </Container>
     </>
--- hemomanager-front/src/pages/HotsitePage/styles.js
+++ hemomanager-front/src/pages/HotsitePage/styles.js
@@ -3,23 +3,22 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding: 1rem;
   margin: auto;
-  margin-top: 1rem;
-  height: 85vh;
+  height: 100vh;
   width: 80vw;
   display: block;
 `;
 
 export const Header = styled.header`
   display: flex;
-  justify-content: start;
   flex-direction: column;
+  margin-left: 87px;
 
   div {
     display: flex;
     align-items: center;
 
     span {
-      font-weight: 700;
+      font-weight: 100;
       color: var(--red-blood);
       font-size: 1.2rem;
     }
@@ -32,7 +31,7 @@ export const Header = styled.header`
 
   p {
     margin-top: 1rem;
-    font-size: 1.4rem;
+    font-size: 1.2rem;
     font-family: "Roboto", sans-serif;
   }
 `;
@@ -40,7 +39,7 @@ export const Header = styled.header`
 export const Title = styled.strong`
   margin-top: 4rem;
   font-family: "Francois One", sans-serif;
-  font-size: 40px;
+  font-size: 35px;
   font-weight: 400;
   color: var(--black);
 `;
@@ -51,7 +50,7 @@ export const InputGroup = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   padding: 0 6rem;
-  height: 70vh;
+  height: 100vh;
   max-width: 980px;
   border-radius: 9px;
   border: 1px solid var(--input-border);
