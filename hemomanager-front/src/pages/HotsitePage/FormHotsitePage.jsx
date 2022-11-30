fc0f5c27ae4f5cfb52567703f3d0ad94964022c7
CindyKanashiro <cindy.goncalves@bandtec.com.br>
2022-11-30 15:33:47 -0300

    inicia integração do cad

--- hemomanager-front/src/pages/HotsitePage/FormHotsitePage.jsx
+++ hemomanager-front/src/pages/HotsitePage/FormHotsitePage.jsx
@@ -5,32 +5,57 @@ import bloodDrop from "../../assets/blood-drop.svg";
 import { Input } from "../../components/shared/Input";
 import { BorderlessButton } from "../../components/shared/BorderlessButton";
 import { useNavigate } from "react-router-dom";
+import { api } from "../../api";
 
 export function FormHotsitePage() {
   const navigate = useNavigate();
 
-  const [name, setName] = useState("XXXXXXXXXXXXXXXXXXXX");
-  const [email, setEmail] = useState("XXXXXXXXXXXXXXXXXXXX");
-  const [phone, setPhone] = useState("XXXXXXXXXXXXXXXXXXXX");
-  const [sex, setSex] = useState("XXXXXXXXXXXXXXXXXXXX");
+  const [name, setName] = useState("Digite seu nome completo aqui!");
+  const [email, setEmail] = useState("Digite seu email aqui!");
+  const [phone, setPhone] = useState("Digite seu telefone aqui!");
+  const [sex, setSex] = useState("Digite seu gênero aqui!");
+  const [cpf, setCpf] = useState("Digite seu CPF aqui!");
+  const [birthDate, setBirthDate] = useState("Digite sua data de nascimento aqui!");
+  const [password, setPassword] = useState("123");
 
+  const [error, setError] = useState(false);
 
   const user = {
     name,
     email,
     phone,
     sex,
+    cpf,
+    birthDate,
+    password
   };
-
-  {
-    console.log(user);
-  }
 
   function doGoToRegister() {
 
     navigate("/upload-hotsite");
 
     sessionStorage.setItem("page", 1);
+  }
+
+  function validateError() {
+    setError(true);
+    setTimeout(() => setError(false), 3000);
+  }
+
+  function sendRegister() {
+    api
+      .post(
+        "/donor",
+        user
+      )
+      .then((resp) => {
+        sessionStorage.setItem("donorId", resp.data.id);
+        doGoToRegister();
+
+      })
+      .catch((erro) => {
+        validateError();
+      });
   }
 
   return (
@@ -52,31 +77,31 @@ export function FormHotsitePage() {
             <Input
               valueInput={name}
               doSomething={setName}
-              placeholderInput={sex}
+              placeholderInput={name}
             />
           </div>
           <div>
             <span>Email</span>
             <Input
-              valueInput={phone}
-              doSomething={setPhone}
-              placeholderInput={sex}
+              valueInput={email}
+              doSomething={setEmail}
+              placeholderInput={email}
             />
           </div>
           <div>
             <span>CPF</span>
             <Input
-              valueInput={email}
-              doSomething={setEmail}
-              placeholderInput={sex}
+              valueInput={cpf}
+              doSomething={setCpf}
+              placeholderInput={cpf}
             />
           </div>
           <div>
             <span>Data de Nascimento</span>
             <Input
-              valueInput={sex}
-              doSomething={setSex}
-              placeholderInput={sex}
+              valueInput={birthDate}
+              doSomething={setBirthDate}
+              placeholderInput={birthDate}
             />
           </div>
           <div>
@@ -90,15 +115,15 @@ export function FormHotsitePage() {
           <div>
             <span>Telefone</span>
             <Input
-              valueInput={sex}
-              doSomething={setSex}
-              placeholderInput={sex}
+              valueInput={phone}
+              doSomething={setPhone}
+              placeholderInput={phone}
             />
           </div>
         </InputGroup>
         <BoxButton>
           <BorderlessButton
-            doSomething={() => doGoToRegister()}
+            doSomething={() => sendRegister()}
             text="ENVIAR"
           />
         </BoxButton>
